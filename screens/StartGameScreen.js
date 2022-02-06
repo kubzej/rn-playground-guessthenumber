13723ad8f9c6dab05847403b139732cc80fce9bb
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-06 11:31:58 +0100

    close keyboard when clicking outside of text input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import colors from '../constants/colors';
@@ -12,28 +19,34 @@ const StartGameScreen = (props) => {
   };
 
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a new game</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a number:</Text>
-        <Input
-          style={styles.input}
-          blurOnSubmit // official props which I am forwarding to TextInput component in my own Input.js
-          keyboardType='number-pad'
-          maxLength={2}
-          onChangeText={numberInputHandler} // when changing text run validation
-          value={enteredValue} // set value as enteredValue which is taken from numberInputHandler
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title='Reset' color={colors.accent} />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a new game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a number:</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit // official props which I am forwarding to TextInput component in my own Input.js
+            keyboardType='number-pad'
+            maxLength={2}
+            onChangeText={numberInputHandler} // when changing text run validation
+            value={enteredValue} // set value as enteredValue which is taken from numberInputHandler
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title='Reset' color={colors.accent} />
+            </View>
+            <View style={styles.button}>
+              <Button title='Confirm' color={colors.primary} />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button title='Confirm' color={colors.primary} />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
