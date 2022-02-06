b3b85848d7d6d0d2275c50ae5107e912b7565262
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-06 15:57:27 +0100

    add GameScreen with function generateRandomBetween

---
+++ screens/GameScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude); // recursive function when computer guess the excluded number
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice) // start game with number between 1 and 100 (100 automatically excluded) and exclude users choice
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
