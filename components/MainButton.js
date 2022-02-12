2a498ca8b7fcb29a5368414cf2a034965c43cfe6
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-12 17:52:22 +0100

    replace lower higher text with icons

--- components/MainButton.js
+++ components/MainButton.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.primary,
     paddingVertical: 20,
     paddingHorizontal: 30,
-    borderRadius: 30,
+    borderRadius: 25,
   },
   buttonText: {
     color: 'white',
