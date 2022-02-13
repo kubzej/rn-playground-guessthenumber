a8a50f2b37b281679911953f8c1237c678d42a1b
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-13 09:43:06 +0100

    work with Dimensions with image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image, Text } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Button,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
 import colors from '../constants/colors';
@@ -7,25 +15,27 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../assets/success.png')}
-          style={styles.image}
-          resizeMode='cover' // default
-        />
-      </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed rounds{' '}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> to guess
-          the number <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/success.png')}
+            style={styles.image}
+            resizeMode='cover' // default
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed rounds{' '}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> to guess
+            the number <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
 
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -36,13 +46,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150, // 300 300 150 makes perfect circle
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2, // half of each widht and height makes perfect circle
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden', // squeeze image into borders, all over is hidden
-    marginVertical: 30,
+    marginVertical: Dimensions.get('window').height / 30,
   },
   image: {
     width: '100%', // percentage of parent view which is 300  of screen
@@ -50,11 +60,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get('window').height / 60,
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
   },
   highlight: {
     color: colors.accent,
